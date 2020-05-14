995eb46dd7f6abcf0d14c4a59fea840c11d3e01c
Lucas Rego <lucas.raziel@gmail.com>
2020-05-14 13:40:54 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-const App: React.FC = () => <View />;
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
 
 export default App;
